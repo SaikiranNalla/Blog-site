6fa2b70e53c897ac03ab608ae117ab3504737679
SaikiranNalla <saikiran26499@gmail.com>
2025-04-05 21:38:49 +0530

    dotenv module error

--- main.py
+++ main.py
@@ -64,8 +64,8 @@ db.init_app(app)
 
 
 # Configure Flask-Mail
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
