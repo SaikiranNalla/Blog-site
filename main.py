43bf4bc4238c65442ce64effc2124d514eff5e73
SaikiranNalla <saikiran26499@gmail.com>
2025-04-08 11:30:10 +0530

    update order of the posts, adjust the subtitle

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text, ForeignKey, Date
+from sqlalchemy import Integer, String, Text, ForeignKey
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
